65840fa6ce4d044470d972c98f6be3dc593a027b
Sleepy_dog <124705401+sleepydog25@users.noreply.github.com>
2024-03-27 18:20:34 +0800

    Create 485.java

---
+++ 485.java
@@ -0,0 +1,42 @@
+//485
+//https://leetcode.com/problems/max-consecutive-ones/
+
+
+class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        List <String> newarr = new ArrayList <>();
+
+        for (int e:nums){
+            newarr.add(Integer.toString(e));
+        }
+
+        /*
+        testing 
+
+        for (String f:newarr){
+            System.out.print(f);
+        }
+        */
+        String buffer =newarr.stream().collect(Collectors.joining(""));
+
+        int count = 0;
+        int maxCount =  0;
+ 
+        // Loop through the binary string to find the longest consecutive 1s
+        for (int i = 0; i < buffer.length(); i++) {
+            if (buffer.charAt(i) == '1') {
+                count++;
+                if (count > maxCount) {
+                    maxCount = count;
+                }
+            } else {
+                count = 0;
+            }
+        }
+ 
+        // Print the result
+        return maxCount;
+
+
+    }
+}
